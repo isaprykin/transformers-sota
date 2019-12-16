88bc87404eade71fa60efb8cd1e94ecfebce63c8
Igor Saprykin <sheleztt@gmail.com>
2019-12-16 08:47:24 -0800

    Fix lint errors in main.py.
    This makes the whole current codebase lint free.

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
+"""Download datasets and train transformer-based models."""
 import argparse
 import logging
 import datasets
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset_directory',
-                        default='/tmp/translation_datasets/',
-                        help='The local directory where the datasets are'
-                        ' stored.')
-    parser.add_argument('--log_level', default='WARNING',
-                        help='The desired logging level.')
-    args = parser.parse_args()
-    logging.basicConfig(level=getattr(logging, args.log_level.upper(), None))
+  """The main entry point to transformers-sota."""
 
-    datasets.obtain(args.dataset_directory)
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--dataset_directory',
+                      default='/tmp/translation_datasets/',
+                      help='The local directory where the datasets are stored.')
+  parser.add_argument('--log_level', default='WARNING',
+                      help='The desired logging level.')
+  args = parser.parse_args()
+  logging.basicConfig(level=getattr(logging, args.log_level.upper(), None))
+
+  datasets.obtain(args.dataset_directory)
 
 
 if __name__ == '__main__':
-    main()
+  main()
